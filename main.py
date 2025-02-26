347e478bd41e7a7cf1c437c0fbbad5bc960e3b4e
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-26 14:13:15 -0300

    colocando como comentário e partindo para exercício

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-print("hello world")
+#print("hello world")
 
-print(3+4)
+#print(3+4)
 
-print("hello" + " " + "world")
+#print("hello" + " " + "world")
 
-print(input("Enter your name:"))
+#print(input("Enter your name:"))
 
 print("Hello, " + input("Enter your name: ")+"!")
