f8a5812233f4c5214813956c763bc77ad9ff30b8
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-26 14:15:38 -0300

    salvando o input

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #print("hello world")
-
 #print(3+4)
-
 #print("hello" + " " + "world")
-
 #print(input("Enter your name:"))
+#print("Hello, " + input("Enter your name: ")+"!")
 
-print("Hello, " + input("Enter your name: ")+"!")
+#Crie um programa que o usuário digita o seu nome e retorna o número de caracteres:
+
+input("Digite seu nome: ")
